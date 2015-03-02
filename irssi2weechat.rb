2ea76be36ef21e981a13994649cfa6e32a2cdc30
Xiagu <xiagu.zepheron@gmail.com>
2015-03-02 18:43:56 -0500

    Fixed indentation and tabs in sample weechat logs
    Converted all indentation to spaces.
    Put tab characters where they're supposed to be in the sample weechat
    logs.

--- irssi2weechat.rb
+++ irssi2weechat.rb
@@ -27,7 +27,7 @@ logfiles.each { |filename|
 
                 # Doesn't catch the user printout upon joining a channel
                 # Doesn't catch topic changes (but we don't do that, lol)
-		# Doesn't catch your own nick changes (logged as "You are now known...")
+                # Doesn't catch your own nick changes (logged as "You are now known...")
                 # msg, action, join, part, quit, nick, mode
 
                 timestamp = /^(\d\d):(\d\d)/
@@ -42,58 +42,59 @@ logfiles.each { |filename|
                 # HH:MM              --> | NICK [USER@HOST] has joined #channel
                 join = /#{timestamp}\s+--> \| #{re_nick} \[([^\]]+)\] has joined #{channel}/
 
-		# HH:MM              <-- | Nick [User@host.name] has left #channel ( "")
+                # HH:MM              <-- | Nick [User@host.name] has left #channel ( "")
                 part = /#{timestamp}\s+<-- \| #{re_nick} \[([^\]]+)\] has left #{channel} \( (.*?)\)/
 
-		# HH:MM              <-- | Nick [User@host.name] has quit (Ping timeout: 121 seconds)
+                # HH:MM              <-- | Nick [User@host.name] has quit (Ping timeout: 121 seconds)
                 quit = /#{timestamp}\s+<-- \| #{re_nick} \[([^\]]+)\] has quit \((.*?)\)/
 
-		# HH:MM              --- | Nick is now known as OtherNick
+                # HH:MM              --- | Nick is now known as OtherNick
                 nick = /#{timestamp}\s+--- \| #{re_nick} is now known as #{re_nick}/
 
                 # HH:MM              --- | ChanServ sets modes [#channel +qo Nick Nick]
                 mode = /#{timestamp}\s+--- \| #{re_nick} sets modes \[#{channel} ([^\]]+)\]/
 
                 # unfortunately we have no second information so we must make it up
-		line =~ timestamp # match the timestamp
-                # $1 is always the timestamp
+                line =~ timestamp # match the timestamp
+                # $1 and $2 are from timestamp
                 output = "#{date} #{$1}:#{$2}:00\t"
+
                 case line
                 when msg
-                    # MSG: ["02", "35", "", "Nick", "sample message text"]
                     puts "MSG: #{$~.captures}" if DEBUG >= 5
-                    # 2014-06-10 11:43:54     @Nick   sample message text
+                    # MSG: ["02", "35", "", "Nick", "sample message text"]
+                    # 2014-06-10 11:43:54	@Nick	sample message text
                     output += "#{$3}#{$4}\t#{$5}"
                 when action
-                    # ACTION: ["02", "36", "Nick", "sample action text"]
                     puts "ACTION: #{$~.captures}" if DEBUG >= 5
-		    # 2014-06-10 12:20:09      *      Nick sample action text
+                    # ACTION: ["02", "36", "Nick", "sample action text"]
+                    # 2014-06-10 12:20:09	 *	Nick sample action text
                     output += " *\t#{$3}#{$4}"
                 when join
-                    # JOIN: ["21", "43", "Nick", "User@host.name", "#channel"]
                     puts "JOIN: #{$~.captures}"  if DEBUG >= 5
-		    # 2014-06-10 00:00:57     -->     Nick (User@host.name) has joined #channel
+                    # JOIN: ["21", "43", "Nick", "User@host.name", "#channel"]
+                    # 2014-06-10 00:00:57	-->	Nick (User@host.name) has joined #channel
                     output += "-->\t#{$3} (#{$4}) has joined #{$5}"
                 when part
-                    # PART: ["21", "34", "Nick", "User@host.name", "#channel", "part message"]
                     puts "PART: #{$~.captures}" if DEBUG >= 5
-		    # 2014-06-10 21:32:40   <-- Nick (user@host.name) has left #channel ("WeeChat 1.0-dev")
+                    # PART: ["21", "34", "Nick", "User@host.name", "#channel", "part message"]
+                    # 2014-06-10 21:32:40	<--	Nick (user@host.name) has left #channel ("WeeChat 1.0-dev")
                     output += "<--\t#{$3} (#{$4}) has left #{$5}"
                     output += " (#{$6})" if $6 != "" # add part message if it exists
                 when quit
-                    # QUIT: ["18", "30", "Nick", "User@host.name", "Quit message"]
                     puts "QUIT: #{$~.captures}" if DEBUG >= 5
-		    # 2014-06-10 18:26:15 <-- Nick (User@host.name) has quit (Quit message)
+                    # QUIT: ["18", "30", "Nick", "User@host.name", "Quit message"]
+                    # 2014-06-10 18:26:15	<--	Nick (User@host.name) has quit (Quit message)
                     output += "<--\t#{$3} (#{$4}) has quit (#{$5})"
                 when nick
-                    # NICK: ["15", "13", "Nick", "OtherNick"]
                     puts "NICK: #{$~.captures}" if DEBUG >= 5
-		    # 2014-06-10 00:24:43     --      Nick is now known as OtherNick
+                    # NICK: ["15", "13", "Nick", "OtherNick"]
+                    # 2014-06-10 00:24:43	--	Nick is now known as OtherNick
                     output += "--\t#{$3} is now known as #{$4}"
                 when mode
-                    # MODE: ["15", "14", "ChanServ", "#channel", "+o Nick"]
                     puts "MODE: #{$~.captures}" if DEBUG >= 5
-		    # 2014-06-10 11:40:38    --  Mode #channel [+o Nick] by ChanServ
+                    # MODE: ["15", "14", "ChanServ", "#channel", "+o Nick"]
+                    # 2014-06-10 11:40:38	--	Mode #channel [+o Nick] by ChanServ
                     output += "--\tMode #{$4} [#{$5}] by #{$3}"
                 else
                     puts "MATCH FAILED FOR LINE: #{line}" if DEBUG >= 5
